0adfa2dc03e9f598778b8cfea2fb4254b68c2dd4
CesarAO11 <143725010+CesarAO11@users.noreply.github.com>
2023-09-05 09:47:29 -0500

    Add files via upload

---
+++ Varable1.java
@@ -0,0 +1,35 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package varable1;
+
+/**
+ *
+ * @author labcca
+ */
+public class Varable1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+  
+    int edad = 0;
+        String nombre = "cesar";
+        String carrera = "tics";
+        String nombreMascota = "yack";
+        String nombreMateria ="Fundamentos de programacion";
+      final int anioNacimiento =2004;
+      edad = 18;
+      System.out.println ("Mi nombre es: " + nombre + " y estudio " + carrera);
+      System.out.println ("mi materia favorita es: " + nombreMateria );
+      System.out.println ("mi mascota favorita es: " + nombreMascota );
+      System.out.println ("mi anio de nacimiento es: " + anioNacimiento );
+      System.out.println ("mi edad es: " + edad );
+     
+            
+    }
+    
+}
