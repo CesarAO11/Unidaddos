a7dff431fab85ccf742d304847ca189e3c57cc12
CesarAO11 <143725010+CesarAO11@users.noreply.github.com>
2023-09-05 09:46:52 -0500

    Add files via upload

---
+++ ListaTareas.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package listatareas;
+
+/**
+ *
+ * @author labcca
+ */
+public class ListaTareas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    String tarea1 = "Investigar que es el metodo cientifico";
+    String tarea2 = "Resolver las siguientes ecuaciones";
+    String tarea3 = "calcula el area del circulo";
+    String tarea4 = "Investigar que es java";
+    String tarea5 = "Resuelve las ecuaciones trigonometricas";
+            
+            
+            System.out.println("La tarea 1 es: " + tarea1);
+            System.out.println("La tarea 2 es: " + tarea2);
+            System.out.println("La tarea 3: " + tarea3);
+            System.out.println("La tarea 4 es: " + tarea4);
+            System.out.println("La tarea 5 es: " + tarea5);
+    }
+    
+}
